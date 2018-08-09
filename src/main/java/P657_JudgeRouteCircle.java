b9afb83d8214f3350a7f052d9ae59612eb2cb43c
WangSai[王赛] <wangsai@xiaomi.com>
2018-08-09 21:16:34 +0800

    P657_JudgeRouteCircle

---
+++ src/main/java/P657_JudgeRouteCircle.java
@@ -0,0 +1,33 @@
+/**
+ * Created by WangSai on 8/9/18
+ */
+public class P657_JudgeRouteCircle {
+    public boolean judgeCircle(String moves) {
+        int row = 0;
+        int colum = 0;
+        for (int i = 0; i < moves.length(); i++) {
+            char step = moves.charAt(i);
+            switch (step) {
+                case 'U':
+                    colum++;
+                    break;
+                case 'D':
+                    colum--;
+                    break;
+                case 'L':
+                    row--;
+                    break;
+                case 'R':
+                    row++;
+                    break;
+                default:
+                    return false;
+            }
+        }
+        if (row == 0 && colum == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
