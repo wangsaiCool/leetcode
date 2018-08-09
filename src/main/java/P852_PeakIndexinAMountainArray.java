6936773dac88ecedf22b403e8c030ea5c3be2577
WangSai <wangsai.cool@gmail.com>
2018-08-10 01:10:18 +0800

    P852_PeakIndexinAMountainArray

---
+++ src/main/java/P852_PeakIndexinAMountainArray.java
@@ -0,0 +1,19 @@
+public class P852_PeakIndexinAMountainArray {
+    public int peakIndexInMountainArray(int[] A) {
+        int index = 0;
+        if (A.length < 3) {
+            return -1;
+        }
+        for (int i = 1; i < A.length -1; i++) {
+            if (A[i - 1] < A[i] && A[i] > A[i + 1]) {
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    public static void main(String[] args) {
+        int[] A = {0,1,0};
+        System.out.println(new P852_PeakIndexinAMountainArray().peakIndexInMountainArray(A));
+    }
+}
