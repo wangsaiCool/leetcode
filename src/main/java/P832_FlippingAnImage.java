f0e4f388ea6ba9a09ee64db7f65edff34dc17e40
WangSai[王赛] <wangsai@xiaomi.com>
2018-08-09 21:40:05 +0800

    P832_FlippingAnImage

---
+++ src/main/java/P832_FlippingAnImage.java
@@ -0,0 +1,33 @@
+
+/**
+ * Created by WangSai on 8/9/18
+ */
+public class P832_FlippingAnImage {
+    public int[][] flipAndInvertImage(int[][] A) {
+        int row = A.length;
+        int colm = A[0].length;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < colm / 2; j++) {
+                int tmp = A[i][j];
+                A[i][j] = A[i][colm - 1 - j];
+                A[i][colm - 1 - j] = tmp;
+            }
+            for (int j = 0; j < colm; j++) {
+                A[i][j] = 1 - A[i][j];
+                System.out.print(A[i][j] + " ");
+            }
+            System.out.println();
+        }
+        return A;
+    }
+
+    public static void main(String[] args) {
+        P832_FlippingAnImage t = new P832_FlippingAnImage();
+        int[][] A = {
+                {1, 1, 0, 0},
+                {1, 0, 0, 1},
+                {0, 1, 1, 1},
+                {1, 0, 1, 0}};
+        t.flipAndInvertImage(A);
+    }
+}
