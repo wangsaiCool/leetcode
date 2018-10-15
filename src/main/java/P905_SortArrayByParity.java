8f5d657233daf33715a5a4512c43f79c8900c6e9
WangSai[王赛] <wangsai@xiaomi.com>
2018-10-16 00:13:32 +0800

    P905_SortArrayByParity

---
+++ src/main/java/P905_SortArrayByParity.java
@@ -0,0 +1,21 @@
+/**
+ * Created by WangSai on 10/16/18.
+ */
+public class P905_SortArrayByParity {
+    public int[] sortArrayByParity(int[] A) {
+        if (A == null || A.length == 0) {
+            return A;
+        }
+        int[] result = new int[A.length];
+        int left = 0;
+        int right = A.length - 1;
+        for (int num : A) {
+            if (num % 2 == 0) {
+                result[left++] = num;
+            } else {
+                result[right--] = num;
+            }
+        }
+        return result;
+    }
+}
