cfc966f0e1ee269ab32a33e099c9f20ec5b590c5
WangSai[王赛] <wangsai@xiaomi.com>
2019-03-16 15:49:47 +0800

    P942_DIStringMatch

---
+++ src/main/java/P942_DIStringMatch.java
@@ -0,0 +1,35 @@
+/**
+ * Created by WangSai on 3/16/19.
+ */
+
+class P942_DIStringMatch {
+    public int[] diStringMatch(String S) {
+        int lo = 0;
+        int hi = S.length();
+        int[] arr = new int[S.length() + 1];
+
+
+        for (int i = 0; i < S.length(); i++) {
+            char letter = S.charAt(i);
+            if (letter == 'I') {
+                arr[i] = lo++;
+            } else {
+                arr[i] = hi--;
+            }
+
+        }
+        arr[S.length()] = lo;
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        String s = "IDID";
+        new P942_DIStringMatch().diStringMatch(s);
+    }
+}
+
+
+
