8fb2f91cc806a8c8ba341d7fa18c1a3a41af9c85
WangSai <wangsai@xiaomi.com>
2019-05-03 00:12:36 +0800

    add comments for P938_RangeSumofBST

--- src/main/java/problems/P938_RangeSumofBST.java
+++ src/main/java/problems/P938_RangeSumofBST.java
@@ -2,6 +2,35 @@ package problems;
 
 import TreeNode.TreeNode;
 
+/**
+ * 题目链接：https://leetcode.com/problems/range-sum-of-bst/
+ *
+ * Given the root node of a binary search tree, return the sum of values of all nodes with value between L and R (inclusive).
+ *
+ * The binary search tree is guaranteed to have unique values.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: root = [10,5,15,3,7,null,18], L = 7, R = 15
+ * Output: 32
+ * Example 2:
+ *
+ * Input: root = [10,5,15,3,7,13,18,1,null,6], L = 6, R = 10
+ * Output: 23
+ *
+ *
+ * Note:
+ *
+ * The number of nodes in the tree is at most 10000.
+ * The final answer is guaranteed to be less than 2^31.
+ * Accepted
+ * 40,087
+ * Submissions
+ * 49,928
+ */
+
 public class P938_RangeSumofBST {
     public int rangeSumBST_1(TreeNode root, int L, int R) {
         int[] ret = new int[1];
