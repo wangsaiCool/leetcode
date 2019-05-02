d188dd06c37a9721fe9c5562fb6dccf5cbb51749
WangSai <wangsai@xiaomi.com>
2019-05-03 00:14:55 +0800

    add comments for P001_TwoSum

--- src/main/java/problems/P001_TwoSum.java
+++ src/main/java/problems/P001_TwoSum.java
@@ -2,6 +2,24 @@ package problems;
 
 import java.util.HashMap;
 
+/**
+ *
+ * 题目链接：https://leetcode.com/problems/two-sum/
+ *
+ *
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ *
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ * Example:
+ *
+ * Given nums = [2, 7, 11, 15], target = 9,
+ *
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ *
+ */
+
 public class P001_TwoSum {
     public int[] twoSum_solution1(int[] nums, int target) {
         if (nums.length <= 0) {
