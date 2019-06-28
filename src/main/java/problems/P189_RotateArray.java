4ae9840a6466b370ac8b1647c62dbc60f6becba1
WangSai[王赛] <wangsai.cool@gmail.com>
2019-06-28 17:56:26 +0800

    P189_RotateArray-反转数组子串

---
+++ src/main/java/problems/P189_RotateArray.java
@@ -0,0 +1,69 @@
+package Problems;
+
+/**
+ * Created by WangSai on 2019-06-27 23:57
+ *
+ * 题目链接：https://leetcode.com/problems/rotate-array/
+ *
+ * 题目描述：
+ * Given an array, rotate the array to the right by k steps, where k is non-negative.
+ *
+ * Example 1:
+ *
+ * Input: [1,2,3,4,5,6,7] and k = 3
+ * Output: [5,6,7,1,2,3,4]
+ * Explanation:                 [1,2,3,4,5,6,7]
+ * rotate 1 steps to the right: [7,1,2,3,4,5,6]
+ * rotate 2 steps to the right: [6,7,1,2,3,4,5]
+ * rotate 3 steps to the right: [5,6,7,1,2,3,4]
+ * Example 2:
+ *
+ * Input: [-1,-100,3,99] and k = 2
+ * Output: [3,99,-1,-100]
+ * Explanation:
+ * rotate 1 steps to the right: [99,-1,-100,3]
+ * rotate 2 steps to the right: [3,99,-1,-100]
+ * Note:
+ *
+ * Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+ * Could you do it in-place with O(1) extra space?
+ */
+
+public class P189_RotateArray {
+
+    //时间复杂度O(N),空间复杂度O(N)
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        int[] result = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            int index = (i + k) % nums.length;
+            result[index] = nums[i];
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = result[i];
+        }
+    }
+
+    //空间复杂度O(1)，通过反转子串
+    public void rotate2(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - k - 1);
+        reverse(nums, nums.length - k, nums.length - 1);
+        reverse(nums, 0, nums.length - 1);
+
+    }
+
+    private void reverse(int[] nums, int left, int right) {
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
+
+    }
+
+}
