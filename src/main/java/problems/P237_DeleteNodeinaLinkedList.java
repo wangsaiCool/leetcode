dff293b01fa2fe61058f4c5b8ed6508a680f9c37
WangSai[王赛] <wangsai.cool@gmail.com>
2019-08-14 10:27:03 +0800

    P237_DeleteNodeinaLinkedList-This is a stupid problem

---
+++ src/main/java/problems/P237_DeleteNodeinaLinkedList.java
@@ -0,0 +1,13 @@
+package problems;
+
+/**
+ * Created by WangSai on 8/14/19
+ *
+ * 题目链接：https://leetcode.com/problems/delete-node-in-a-linked-list/
+ *
+ * 题目描述：删除单链表里面指定的节点. This is a very stupid problem.
+ *
+ */
+public class P237_DeleteNodeinaLinkedList {
+    //This is a very stupid problem.
+}
