a0b27dad58924944bc419abfe858416d41799dd5
WangSai[王赛] <wangsai.cool@gmail.com>
2019-11-16 23:09:23 +0800

    add another two solutions

--- src/main/java/problems/P021_MergeTwoSortedLists.java
+++ src/main/java/problems/P021_MergeTwoSortedLists.java
@@ -13,11 +13,29 @@ public class P021_MergeTwoSortedLists {
     /**
      * Definition for singly-linked list.
      * public class ListNode {
-     * int val;
-     * ListNode next;
-     * ListNode(int x) { val = x; }
+     *   int val;
+     *   ListNode next;
+     *   ListNode(int x) { val = x; }
      * }
      */
+    public ListNode mergeTwoLists3(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists3(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists3(l1, l2.next);
+            return l2;
+        }
+
+    }
+
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if (l1 == null) {
             return l2;
@@ -109,23 +127,6 @@ public class P021_MergeTwoSortedLists {
         return head;
     }
 
-    public ListNode mergeTwoLists3(ListNode l1, ListNode l2) {
-        if (l1 == null) {
-            return l2;
-        }
-        if (l2 == null) {
-            return l1;
-        }
-
-        if (l1.val < l2.val) {
-            l1.next = mergeTwoLists3(l1.next, l2);
-            return l1;
-        } else {
-            l2.next = mergeTwoLists3(l1, l2.next);
-            return l2;
-        }
-
-    }
 
     public static void main(String[] args) {
         ListNode l1 = new ListNode(1);
