b8f21cd1cdcac562019478ea927a3c362c4f8e53
WangSai[王赛] <wangsai.cool@gmail.com>
2019-11-16 16:56:21 +0800

    merge two sorted linked list

--- src/main/java/problems/P021_MergeTwoSortedLists.java
+++ src/main/java/problems/P021_MergeTwoSortedLists.java
@@ -1,4 +1,4 @@
-package problems;
+package Problems;
 
 import myUtils.ListNode;
 
@@ -7,7 +7,9 @@ import myUtils.ListNode;
  *
  * Created by WangSai on 11/15/19
  */
+
 public class P021_MergeTwoSortedLists {
+
     /**
      * Definition for singly-linked list.
      * public class ListNode {
@@ -24,16 +26,62 @@ public class P021_MergeTwoSortedLists {
             return l1;
         }
 
-        ListNode head = new ListNode(0);
-        ListNode newListNode = head;
-        while (l1 != null && l2 != null) {
+        ListNode head;
+        if (l1.val < l2.val) {
+            head = l1;
+            l1 = l1.next;
+        }else{
+            head = l2;
+            l2 = l2.next;
+        }
 
+        ListNode pre = head;
+        while (l1 != null && l2 != null) {
             if (l1.val < l2.val) {
-                newListNode = new ListNode(l1.val);
+                pre.next = l1;
                 l1 = l1.next;
             }else{
-
+                pre.next = l2;
+                l2 = l2.next;
             }
+            pre = pre.next;
+        }
+
+        while (l1 != null) {
+            pre.next = l1;
+            pre = pre.next;
+            l1 = l1.next;
+        }
+
+        while (l2 != null) {
+            pre.next = l2;
+            pre = pre.next;
+            l2 = l2.next;
+        }
+
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(2);
+        ListNode l3 = new ListNode(3);
+        ListNode l4 = new ListNode(4);
+        l1.next = l2;
+        l2.next = l3;
+        l3.next = l4;
+
+        ListNode r10 = new ListNode(10);
+        ListNode r11 = new ListNode(11);
+        ListNode r12 = new ListNode(12);
+        r10.next = r11;
+        r11.next = r12;
+
+        P021_MergeTwoSortedLists test = new P021_MergeTwoSortedLists();
+        ListNode ret = test.mergeTwoLists(l1, r10);
+        while (ret != null) {
+            System.out.println(ret.val);
+            ret = ret.next;
         }
     }
 }
