b3252c17586c30a0378b165d97f3734673b4316b
WangSai <wangsai.cool@gmail.com>
2021-03-27 21:35:07 +0800

    mv patterns to another project

--- src/main/java/art/of/design/patterns/工厂模式/工厂方法模式/Logger.java
+++
@@ -1,8 +0,0 @@
-package art.of.design.patterns.工厂模式.工厂方法模式;
-
-/**
- * Created by WangSai on 2021/3/26
- */
-public interface Logger {
-    public abstract void writeLog(String content);
-}
--- src/main/java/art/of/design/patterns/工厂模式/简单工厂模式/Chart.java
+++
@@ -1,8 +0,0 @@
-package art.of.design.patterns.工厂模式.简单工厂模式;
-
-/**
- * Created by WangSai on 2021/3/25
- */
-public interface Chart {
-    public void display();
-}
